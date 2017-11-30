9e62e944f4668e0029963d8233cde827cf743c07
chenzhiyuan <chenzy@sevencent.com.cn>
2017-11-30 14:41:12 +0800

    modify main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,81 +1,84 @@
 $(function () {
-    initMeters();
-    initIscroll();
-    initCursor();
+  initMeters();
+  initIscroll();
+  initCursor();
 });
 
 function initMeters() {
-    var scaleValue = 141;
-    var html = "";
-    for (var a = 0; a < 61; a++) {
-        html +=
-            '<li>' +
-            '<div class="scale">' +
-            '<div class="scale-line-short"></div>' +
-            '<div class="scale-space"></div>' +
-            '<div class="scale-line-short"></div>' +
-            '<div class="scale-space"></div>' +
-            '<div class="scale-line"></div>' +
-            '<div class="scale-space"></div>' +
-            '<div class="scale-line-short"></div>' +
-            '<div class="scale-space"></div>' +
-            '</div>' +
-            '<div id="' + a + '">' + (scaleValue++) + '</div>' +
-            '<input type="hidden" value="' + (scaleValue - 1) + '">' +
-            '</li>';
-    }
-    $("#liStart").after(html);
+  var scaleValue = 141;
+  var item = [];
+  for (var a = 0; a < 61; a++) {
+    item[a] =
+      '<li>' +
+      '<div class="scale">' +
+      '<div class="scale-line-short"></div>' +
+      '<div class="scale-space"></div>' +
+      '<div class="scale-line-short"></div>' +
+      '<div class="scale-space"></div>' +
+      '<div class="scale-line"></div>' +
+      '<div class="scale-space"></div>' +
+      '<div class="scale-line-short"></div>' +
+      '<div class="scale-space"></div>' +
+      '</div>' +
+      '<div id="' + a + '">' + (scaleValue++) + '</div>' +
+      '<input type="hidden" value="' + (scaleValue - 1) + '">' +
+      '</li>';
+    item.join();
+  }
+
+  $("#liStart").after(item);
 }
 
 var myScroll;
+
 function initIscroll() {
-    myScroll = new IScroll('#wrapper', {
-        eventPassthrough: true,
-        scrollX: true,
-        scrollY: false,
-        preventDefault: false
+  myScroll = new IScroll('#wrapper', {
+    eventPassthrough: true,
+    scrollX: true,
+    scrollY: false,
+    preventDefault: false
 
-    });
-    //监听滚动结束事件
-    myScroll.on('scrollEnd', function () {
-        var num = $('#scroller li').length;
-        var position = $('.cursor').offset().left;
-        var disArray = [];
-        var disValue = [];
-        for (var a = 0; a < num - 1; a++) {
-            var scaleValue = $('#' + a).text();
-            var scalePosition = $('#' + a).offset().left;
-            var distances = scalePosition - position;
-            distances = Math.abs(distances);
-            disArray.push(distances);
-            disValue.push(scaleValue);
-        }
-        //获取数组中最小值的索引
-        var index = indexOfSmallest(disArray);
+  });
+  //监听滚动结束事件
+  myScroll.on('scrollEnd', function () {
+    var num = $('#scroller li').length;
+    var position = $('.cursor').offset().left;
+    var disArray = [];
+    var disValue = [];
+    for (var a = 0; a < num - 1; a++) {
+      var scaleValue = $('#' + a).text();
+      var scalePosition = $('#' + a).offset().left;
+      var distances = scalePosition - position;
+      distances = Math.abs(distances);
+      disArray.push(distances);
+      disValue.push(scaleValue);
+    }
+    //获取数组中最小值的索引
+    var index = indexOfSmallest(disArray);
 
-        $("#bodyHeight").text(disValue[index]);
-        metersAdsorption();
-    });
+    $("#bodyHeight").text(disValue[index]);
+    metersAdsorption();
+  });
 
 }
 
 //初始游标指向160
 function initCursor() {
-    var height = 160;
-    var te = $("input[value='160']").prev().attr('id');
-    myScroll.scrollToElement(document.getElementById(te), 0, true, null);
-    $("#bodyHeight").text(height);
+  var height = 160;
+  var te = $("input[value='160']").prev().attr('id');
+  myScroll.scrollToElement(document.getElementById(te), 0, true, null);
+  $("#bodyHeight").text(height);
 }
 
 function indexOfSmallest(a) {
-    return a.indexOf(Math.min.apply(Math, a));
+  return a.indexOf(Math.min.apply(Math, a));
 }
 
 //米尺吸附
 function metersAdsorption() {
-    var bodyHeight = $("#bodyHeight").text();
-    var el = $('input[value="' + bodyHeight + '"]').prev().attr('id');
-    myScroll.scrollToElement(document.getElementById(el), 0, true, null);
+  var bodyHeight = $("#bodyHeight").text();
+  var el = $('input[value="' + bodyHeight + '"]').prev().attr('id');
+  myScroll.scrollToElement(document.getElementById(el), 0, true, null);
 }
 
 
